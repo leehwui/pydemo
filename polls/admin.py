889dfc2460f7a03f826f29419eebfc361dff9912
leehwui <leehwui@163.com>
2015-09-17 17:46:33 +0800

    Deleted __pycache__

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Question
+from .models import Choice
 
 # Register your models here.
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+            (None, {'fields': ['question_text']}),
+            ('Date information', {'fields': ['pub_date'], 'classes':
+                ['collapse']}),
+            ]
+
+admin.site.register(Question, QuestionAdmin)
+
